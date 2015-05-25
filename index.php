a5f3432044cb2d173f322c986eb6c8e45aef231f
trollderiu <trollderiu@gmail.com>
2015-05-25 17:49:20 +0200

    0

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if (isset($_SESSION['gameId'])) {
     echo "<script>;var gameId = " . $_SESSION['gameId'] . ";</script>";
 }
 
-echo "<script>smalltownURL = $smalltownURL</script>";
+echo "<script>smalltownURL = '$smalltownURL'</script>";
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
