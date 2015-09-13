48f5bd5ee2811ddaa9ff0c3875cb1ee65b1f04bb
trollderiu <trollderiu@gmail.com>
2015-09-13 13:38:39 +0200

    0

--- social/facebook.js
+++ social/facebook.js
@@ -1,22 +1,10 @@
 
-// Load the SDK asynchronously
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id))
-        return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 //auto-init
 window.fbAsyncInit = function () {
 
     FB.init({
         appId: '1572792739668689',
-        cookie: true, // enable cookies to allow the server to access 
-        // the session
+//        cookie: true, // enable cookies to allow the server to access the session
         xfbml: true, // parse social plugins on this page
         version: 'v2.4'
     });
@@ -29,6 +17,17 @@ window.fbAsyncInit = function () {
         SMLTOWN.Social.facebook.statusChangeCallback(response);
     });
 };
+
+// Load the SDK asynchronously
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id))
+        return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 if (typeof SMLTOWN == "undefined") {
     SMLTOWN = {};
